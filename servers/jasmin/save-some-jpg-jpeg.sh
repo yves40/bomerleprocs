9c975701fa330db80cab1bb13f435af88131131a
yves40 <yves.toubhans@free.fr>
2024-01-18 17:40:39 +0100

    Replaced by  single file

--- servers/jasmin/save-some-jpg-jpeg.sh
+++
@@ -1,2 +0,0 @@
-cd $HOME/DEV/bomerle/public
-zip -q $HOME/BACKUP/some-jpg-jpeg.zip images/*.jpg images/*.jpeg
--- servers/jasmin/save-webp-gif-svg.sh
+++
@@ -1,2 +0,0 @@
-cd $HOME/DEV/bomerle/public
-zip -@ -qr  $HOME/BACKUP/webp-gif-svg.zip -x "*.jpg" "*.jpeg" "images/knife/le_*/" "images/knife/l_*" < $DEV/servers/jasmin/webp-gif-svg.list
