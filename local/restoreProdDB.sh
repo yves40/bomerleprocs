83525c3a7f0f963d8560adfa005300246cb0c3ac
yves40 <yves.toubhans@free.fr>
2024-07-18 12:03:57 +0200

    WIP for mysql proc

--- local/restoreProdDB.sh
+++ local/restoreProdDB.sh
@@ -23,10 +23,20 @@ echo; ls -l $1/*.sql
 while [ "$DBFILE" = "" ]
 do
   echo; DBFILE=`./ask.sh "Which sql file ? "`
+  if ! [ -f $DBFILE ]
+  then
+    echo "Please provide a valid file location "
+    DBFILE=""
+  fi
 done
 while [ "$TARGET" = "" ]
 do
   echo; TARGET=`./ask.sh "Target DB ? " "$2"`
 done
+log "mysql --user=$MSQLUSER --password=$MSQLPASSWORD $TARGET"
+mysql --user=$MSQLUSER --password=$MSQLPASSWORD $TARGET << EOF
+source $DBFILE;
+exit;
+EOF
 log "Restore export file $DBFILE to $TARGET database"
 echo "$TARGET" > todelete.data
