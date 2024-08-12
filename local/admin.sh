93748986272c818bc2e22ab031633ad326ac69e8
yves40 <yves.toubhans@free.fr>
2024-08-12 12:37:45 +0200

    WIP on full PROD backup

--- local/admin.sh
+++ local/admin.sh
@@ -3,7 +3,7 @@
 #---------------------------------------------------------------------------------------
 #   Params
 #---------------------------------------------------------------------------------------
-version="admin.sh, Aug 11 2024 : 1.15 "
+version="admin.sh, Aug 12 2024 : 1.19 "
 #---------------------------------------------------------------------------------------
 #   Some parameters
 #---------------------------------------------------------------------------------------
@@ -64,19 +64,20 @@ menu()
   echo "-------------------------------------------------------------------------------"
   echo " L O C A L    A C T I O N S"
   echo "-------------------------------------------------------------------------------"
-  echo "  10 / ListDBbackups        List the available PROD DB backups"
-  echo "  11 / ListImagesbackups    List the available PROD images backups"
-  echo "  12 / ListAllbackups       List all DB and images backups"
+  echo "  10 / List the available PROD DB backups"
+  echo "  11 / List the available PROD images backups"
+  echo "  12 / List all DB and images backups"
   echo
-  echo "  20 / RestoreProdDB        Restore PROD DB in local mysql"
-  echo "  21 / RestoreProdImages    Restore PROD images in local WEB environment"
+  echo "  20 / Restore PROD DB in local mysql"
+  echo "  21 / Restore PROD images in local WEB environment"
   echo
   echo 
   echo "-------------------------------------------------------------------------------"
   echo " R E M O T E   A C T I O N S"
   echo "-------------------------------------------------------------------------------"
-  echo "  50 / getPRODDBcopy        Get a copy of the PROD database"
-  echo "  51 / getPRODImagescopy    Get a copy of all PROD images"
+  echo "  50 / Get a copy of the PROD database"
+  echo "  51 / Get a copy of all PROD images"
+  echo "  52 / Get a full copy of PROD site ( DB and images )"
   echo
   echo "-------------------------------------------------------------------------------"
   echo " L O G S"
@@ -91,28 +92,6 @@ menu()
 parsecommand() {
   command=`echo $1 | tr A-Z a-z`
   case "$command" in 
-    '20')     
-                echo;echo
-                ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
-                if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
-                then
-                  ./restoreProdDB.sh $LOCALBACKUPDIR $LOCALTARGETDB
-                  tdb=$(cat todelete.data); rm todelete.data
-                  feedback "PROD DB restored in local database : $tdb" "y"
-                  LOCALTARGETDB="$tdb"
-                  export LOCALTARGETDB="$tdb"
-                fi
-                ;;
-    '21')
-                echo
-                echo; ls -l $LOCALBACKUPDIR/*.gz; echo; echo
-                ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
-                if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
-                then
-                  log "Restoring the PROD images in $LOCALWEBDIR/images"
-                  feedback "PROD images restored in local environment"
-                fi
-                ;;    
     '10')
                 echo; ls -l $LOCALBACKUPDIR/*.sql
                 ;;
@@ -122,21 +101,17 @@ parsecommand() {
     '12')
                 echo; ls -l $LOCALBACKUPDIR
                 ;;
+    '20')       RestoreDB "PROD"
+                ;;
+    '21')       RestoreProdImages
+                ;;    
     '50')
-                echo;echo
-                ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
-                if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
-                then
-                  getPRODDBcopy
-                fi
+                getPRODDBcopy
                 ;;
     '51')
-                echo;echo
-                ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
-                if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
-                then
-                  getPRODImagescopy
-                fi
+                getPRODImagescopy
+                ;;
+    '52')       getPRODFull
                 ;;
     'log')      echo
                 less $O2logs
@@ -150,46 +125,162 @@ parsecommand() {
   echo; ANSWER=`./ask.sh "Back to menu <CR> "`
 }
 #---------------------------------------------------------------------------------------
-#   Get a backup of PROD db 
+#   S U B   R O U T I N E S
 #---------------------------------------------------------------------------------------
-getPRODImagescopy () {
-  ssh -x "$O2USER@$O2HOST" <<-EOF
-    echo;echo;echo "File root will be : $DATESIGNATURE"
-    echo; ls -l ~/BACKUP
-    echo; ls -l $PROD/public/images
-    DATESIGNATURE=`date +"%Y-%m-%d"`
-    if [ -f BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz ]
+#   Restore PROD DB backup 
+#   $1 is the DB type : PROD or DEV
+#---------------------------------------------------------------------------------------
+RestoreDB() {
+  echo;echo
+  ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
+  if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
+  then
+    DBFILE=""
+    TARGET=""
+    initdir=`pwd`
+    echo; ls -l $LOCALBACKUPDIR/*$1*.sql
+    while [ "$DBFILE" = "" ]
+    do
+      echo; DBFILE=`./ask.sh "Which sql file ? "`
+      if ! [ -f $DBFILE ]
+      then
+        echo "Please provide a valid file location "
+        echo $DBFILE
+        DBFILE=""
+      fi
+    done
+    while [ "$TARGET" = "" ]
+    do
+      echo; TARGET=`./ask.sh "Target DB ? " "$LOCALTARGETDB"`
+    done
+    # Ensure we do not have special MYSQL export directive on line 1
+    # If so remove it
+    grep 99999 $DBFILE > todelete
+    if [ $? == 0 ]
     then
-      rm BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz
+      echo "************ Stripped"
+      tail -n +2 $DBFILE > "$DBFILE.strip"
+      cp "$DBFILE.strip" $DBFILE
+      rm "$DBFILE.strip"
     fi
-    echo;echo "Backup all PROD images";echo
-    initdir=$(pwd)
-    cd $PROD/public
-    tar czvf ~/BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz images
-    cd $initdir
-    echo; ls -l ~/BACKUP
+    rm todelete
+    # Restore the SQL file into the target DB
+    echo
+    echo
+    echo "Restore now : $DBFILE"
+    mysql --user=$MSQLUSER --password=$MSQLPASSWORD $TARGET  << EOF
+      set autocommit=0;
+      source $DBFILE ;
+      commit;
+      exit
 EOF
-  echo;echo "scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz ~/bomerleprocs/backups";echo
-  scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz ~/bomerleprocs/backups
+
+    export LOCALTARGETDB="$TARGET"
+    cd $initdir
+  fi
 }
 #---------------------------------------------------------------------------------------
-#   Get a backup of PROD db 
+#   Restore PROD images 
+#---------------------------------------------------------------------------------------
+RestoreProdImages () {
+  echo
+  ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
+  if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
+  then
+    echo; ls -l $LOCALBACKUPDIR/*.gz; echo; echo
+    # Choose the compressed archive
+    GZFILE=""
+    while [ "$GZFILE" = "" ]
+    do
+      echo; GZFILE=`./ask.sh "Which archive file ? "`
+      if ! [ -f $GZFILE ]
+      then
+        echo "Please provide a valid file location "
+        echo $GZFILE
+        GZFILE=""
+      fi
+    done
+    # Now proceed
+    initdir=$(pwd)
+    cd $LOCALWEBDIR/public
+    log "Restoring the PROD images in $LOCALWEBDIR/public/images"
+    tar xzvf $GZFILE
+    cd $initdir
+    feedback "PROD images restored in local environment"
+  fi
+}
+#---------------------------------------------------------------------------------------
+#   Get Full PROD site copy 
+#---------------------------------------------------------------------------------------
+getPRODFull() {
+  echo;echo
+  ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
+  if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
+  then
+    getPRODDBcopy "nointeractive"
+    getPRODImagescopy "nointeractive"
+    echo; ls -l $LOCALBACKUPDIR/*PROD*;echo
+  fi
+}
+#---------------------------------------------------------------------------------------
+#   Get PROD images 
+#   $1 'noninteractive' means confirmation has been already done
+#---------------------------------------------------------------------------------------
+getPRODImagescopy () {
+  echo;echo
+  if [ -z $1 ]
+  then
+    ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
+  else
+    ANSWER='Y'
+  fi
+  if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
+  then
+    ssh -x "$O2USER@$O2HOST" <<-EOF
+      echo;echo;echo "File root will be : $DATESIGNATURE"
+      echo; ls -l ~/BACKUP
+      echo; ls -l $PROD/public/images
+      DATESIGNATURE=`date +"%Y-%m-%d"`
+      if [ -f BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz ]
+      then
+        rm BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz
+      fi
+      echo;echo "Backup all PROD images";echo
+      initdir=$(pwd)
+      cd $PROD/public
+      tar czvf ~/BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz images
+      cd $initdir
+EOF
+    scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-ALLIMAGES.tar.gz ~/bomerleprocs/backups
+  fi
+}
+#---------------------------------------------------------------------------------------
+#   Get PROD db
+#   $1 'noninteractive' means confirmation has been already done
 #---------------------------------------------------------------------------------------
 getPRODDBcopy () {
-  ssh -x "$O2USER@$O2HOST" <<-EOF
-    echo;echo;echo "File root will be : $DATESIGNATURE"
-    echo; ls -l ~/BACKUP
-    DATESIGNATURE=`date +"%Y-%m-%d"`
-    if [ -f BACKUP/$DATESIGNATURE-toba3789_PRODbomerle.sql ]
-    then
-      rm BACKUP/$DATESIGNATURE-toba3789_PRODbomerle.sql
-    fi
-    echo;echo "Connect as $SQLUSER on $SQLPRODDB";echo
-    mysqldump -u $SQLUSER --password=$SQLPASS --result-file=BACKUP/$DATESIGNATURE-$SQLPRODDB.sql $SQLPRODDB
-    echo; ls -l ~/BACKUP
+  echo;echo
+  if [ -z $1 ]
+  then
+    ANSWER=`./ask.sh "Proceed ? Y/N <CR> " "N"`
+  else
+    ANSWER='Y'
+  fi
+  if [ `echo $ANSWER | tr A-Z a-z` == "y" ] 
+  then
+    ssh -x "$O2USER@$O2HOST" <<-EOF
+      echo;echo;echo "File root will be : $DATESIGNATURE"
+      echo; ls -l ~/BACKUP
+      DATESIGNATURE=`date +"%Y-%m-%d"`
+      if [ -f BACKUP/$DATESIGNATURE-toba3789_PRODbomerle.sql ]
+      then
+        rm BACKUP/$DATESIGNATURE-toba3789_PRODbomerle.sql
+      fi
+      echo;echo "Connect as $SQLUSER on $SQLPRODDB";echo
+      mysqldump -u $SQLUSER --password=$SQLPASS --result-file=BACKUP/$DATESIGNATURE-$SQLPRODDB.sql $SQLPRODDB
 EOF
-  echo;echo "scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-$SQLPRODDB.sql ~/bomerleprocs/backups";echo
-  scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-$SQLPRODDB.sql ~/bomerleprocs/backups
+    scp $O2USER@$O2HOST:BACKUP/$DATESIGNATURE-$SQLPRODDB.sql ~/bomerleprocs/backups
+  fi
 }
 #---------------------------------------------------------------------------------------
 #   S T A R T   H E R E
